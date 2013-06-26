dda4a4c852ebd4c69c9aa18570780e78905b985e
App Academy Student <student@appacademy.io>
2013-06-25 17:27:56 -0700

    added tag form that works

--- app/assets/javascripts/controllers/taggingController.js
+++ app/assets/javascripts/controllers/taggingController.js
@@ -10,12 +10,17 @@ PA.Controllers.TagFormController = function($rootEl, x_coord, y_coord, friends) 
 }
 
 PA.Controllers.TagFormController.prototype.render = function () {
-	console.log("x_coord: " + this.x_coord);
+	var formDiv = $(".form_div");
+	formDiv.addClass("new_tag_form");
+	formDiv.css({
+							"left": this.x_coord + "px",
+							"top": this.y_coord + "px"});
+
 	var renderedTagForm = JST["tags/new_tag"]({
 		friends: this.friends,
 		x_pos: this.x_coord,
 		y_pos: this.y_coord
 	});
 
-	this.$rootEl.append(renderedTagForm);
+	formDiv.html(renderedTagForm);
 }
